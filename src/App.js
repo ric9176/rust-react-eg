2a15fbd3360b1fc93ac14d165037955e8457ef99
Richard Moss <richard@englishtutorslive.com>
2018-09-09 20:04:00 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+/* global WebAssembly */
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    const importObject = { imports: { imported_func: arg => console.log(arg) } }
+    WebAssembly.instantiateStreaming(fetch('number_adder.wasm', importObject)).then((obj) => {
+      console.log(obj)
+    })
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
